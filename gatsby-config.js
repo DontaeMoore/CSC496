8c55315e4f58594e1a46c74637a682db4c97157c
DontaeMoore <dontae.moore1023@gmail.com>
2022-03-01 16:52:40 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `This is for today`,
+      title: `CSC 496 Project`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
       "gatsby-plugin-image",
       "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
